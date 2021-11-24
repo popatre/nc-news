3719c4950e6c6cf974b346c75eebdf59aac21497
popatre <76402245+popatre@users.noreply.github.com>
2021-11-24 16:48:52 +0000

    adds error handling for article id not existing

--- src/views/ArticleView.js
+++ src/views/ArticleView.js
@@ -2,15 +2,28 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ArticleCard from "../components/ArticleCard";
 import { getArticleById } from "../utils/api";
+import ErrorPage from "../views/ErrorPage";
 
 export default function ArticleView() {
     const { article_id } = useParams();
     const [article, setArticle] = useState([]);
+    const [error, setError] = useState(null);
     useEffect(() => {
-        getArticleById(article_id).then((article) => {
-            setArticle([article]);
-        });
+        getArticleById(article_id)
+            .then((article) => {
+                setArticle([article]);
+            })
+            .catch((err) => {
+                const message = err.response.data.message;
+                const errorCode = err.response.status;
+                setError({ errorCode, message });
+            });
     }, [article_id]);
+
+    if (error)
+        return (
+            <ErrorPage errorCode={error.errorCode} message={error.message} />
+        );
 
     return (
         <div className="article__single">
