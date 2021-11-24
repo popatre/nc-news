395e5e969761e106da3bc8e7e483ece9f26afa78
popatre <76402245+popatre@users.noreply.github.com>
2021-11-24 14:54:11 +0000

    adds sort label

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,3 @@
+export default function DeleteButton({ id }) {
+    return <button>Delete</button>;
+}
--- src/components/SortBy.js
+++ src/components/SortBy.js
@@ -1,16 +1,19 @@
 import { Dropdown } from "react-bootstrap";
+import { useState } from "react";
 
 export default function SortBy({ setSort }) {
+    const [sortLabel, setSortLabel] = useState();
     const handleChange = (e) => {
-        const { name } = e.target;
+        const { name, innerText } = e.target;
 
+        setSortLabel(innerText);
         setSort(name);
     };
 
     return (
         <Dropdown>
             <Dropdown.Toggle variant="warning" id="dropdown-basic">
-                Sort by:
+                {!!sortLabel ? sortLabel : "Sort by:"}
             </Dropdown.Toggle>
             <Dropdown.Menu onClick={handleChange}>
                 <Dropdown.Item name="created_at" value="created_at">
