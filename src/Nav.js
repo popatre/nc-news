78ecda0d4c8dab9c28ae5045a84ef97304632124
popatre <76402245+popatre@users.noreply.github.com>
2021-11-25 15:18:30 +0000

    styles basic nav

--- src/Nav.js
+++ src/Nav.js
@@ -10,7 +10,7 @@ export default function NavbarComp() {
 
     return (
         <nav>
-            <Navbar bg="warning" expand="lg">
+            <Navbar bg="warning" expand="lg" className="nav__container">
                 <Navbar.Brand>
                     <Link to="/" className="Nav__link">
                         NC News <i className="fas fa-globe-americas fa-2x"></i>
@@ -23,17 +23,20 @@ export default function NavbarComp() {
 
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Container>
-                        <Nav className="justify-content-end">
-                            <Nav.Item>
-                                {/* <Nav.Link eventKey="createTopic"> */}
+                        <Nav className="justify-content-end nav__links">
+                            {/* <Nav.Item>
+                                
                                 <Link to="/newTopic" className="Nav__link">
                                     Add a topic
                                 </Link>
-                                {/* </Nav.Link> */}
-                            </Nav.Item>
+                        
+                            </Nav.Item> */}
                             {!!user.username ? (
                                 <Nav.Item>
-                                    <Link to="/account" className="Nav__link">
+                                    <Link
+                                        to="/account"
+                                        className="Nav__link--signedin"
+                                    >
                                         <img
                                             className="Navbar__avatar"
                                             src={user.avatar_url}
@@ -44,9 +47,12 @@ export default function NavbarComp() {
                                 </Nav.Item>
                             ) : (
                                 <Nav.Item>
-                                    <Link to="/account" className="Nav__link">
-                                        <FaceIcon />
+                                    <Link
+                                        to="/account"
+                                        className="Nav__link--signin"
+                                    >
                                         <p>Sign in</p>
+                                        <FaceIcon className="Nav__link--signin__icon" />
                                     </Link>
                                 </Nav.Item>
                             )}
